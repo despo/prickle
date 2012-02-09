d678f6c36d3af97cb8c3794cdcf083d9af73cc31
Despo Pentara <despo@despo.me>
2012-02-09 16:55:49 +0000

    use find_by_name

--- lib/prickle/capybara/find.rb
+++ lib/prickle/capybara/find.rb
@@ -2,14 +2,14 @@ module Prickle
   module Actions
     module Find
 
-      def find_by_name name
-        find(:xpath, "//*[@name='#{name}']")
+      def find_by_name type='*', name
+        find(:xpath, "//#{type}[@name='#{name}']")
       end
 
       def method_missing method, *args
         if method =~ /^find_(.*)_by_name$/
           element = Prickle::TAGS[$1.to_sym] || $1
-          find(:xpath, "//#{element}[@name='#{args.first}']")
+          find_by_name element, args.first
         else
           super
         end
